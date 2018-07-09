2278b23c1862c7e3be03b50eb41f99e378dd4b4a
ebinks <elizabethjbinks@gmail.com>
2018-07-09 17:06:51 -0400

    implemented polling for logs from starting block

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"time"
 	"jsonparser"
+	"encoding/json"
+	//"encoding/hex"
 )
 
 type Resp struct {
@@ -16,38 +18,64 @@ type Resp struct {
 	result string
 }
 
+type Call struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Method string `json:"method"`
+	Params []string `json:"params"`
+	Id int `json:"id"`
+}
+
+type LogParams struct {
+	FromBlock string `json:"fromBlock"`
+}
+
 func main() {
 	// hard coded to geth running at address:port
 	url := "http://127.0.0.1:8545"
 
-	var jsonStr = []byte(`{"jsonrpc":"2.0","method":"eth_newBlockFilter","params":[],"id":71}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
+	// get starting block
+	var jsonBytes = []byte(`{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}`)
+        req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
+        req.Header.Set("Content-Type", "application/json")
+        client := &http.Client{}
+        blockNumResp, err := client.Do(req)
+        if err != nil {
+                fmt.Println(err)
+        }
+        defer blockNumResp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
-	jsonBody := []byte(string(body))
+	// print out response of eth_blockNumber
+	fmt.Println("response Status:", blockNumResp.Status)
+	fmt.Println("response Headers:", blockNumResp.Header)
+	blockNumBody, _ := ioutil.ReadAll(blockNumResp.Body)
+	fmt.Println("response Body:", string(blockNumBody))
 
-	var res, _, _,  _ = jsonparser.Get(jsonBody, "result")
-	fmt.Println("filter result: %s\n", string(res))
+	// parse json for result
+	jsonBody := []byte(string(blockNumBody))
+	res, _, _, _ := jsonparser.Get(jsonBody, "result")
+	//resStr := "0x" + hex.EncodeToString(res)
+	startBlock := string(res)
+	fmt.Println("starting block number: " + startBlock + "\n")
 
-        jsonStr = []byte(`{"jsonrpc":"2.0","method":"eth_getFilterChanges","params":[],"id":71}`)
+	//format json call
+	var params LogParams
+	//params.FromBlock = "0x1"
+	params.FromBlock = startBlock
+	jsonParams, _ := json.Marshal(params)
+	fmt.Println(string(jsonParams))
+
+	jsonStr := `{"jsonrpc":"2.0","method":"eth_getLogs","params":[` + string(jsonParams) + `],"id":74}`
+	jsonBytes = []byte(jsonStr)
+	fmt.Println(string(jsonBytes))
+	// poll filter every 500ms for changes
 	ticker := time.NewTicker(500 * time.Millisecond)
 	go func() {
 		for t := range ticker.C {
 			fmt.Println(t)
-			req, _ = http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+			req, _ = http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
 			req.Header.Set("Content-Type", "application/json")
 			resp, err := client.Do(req)
-        		if err != nil {
+ 			if err != nil {
                 		fmt.Println(err)
         		}
    			defer resp.Body.Close()
