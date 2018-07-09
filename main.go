a1446a610929c13982756d6b42c481d4e7f0f454
ebinks <elizabethjbinks@gmail.com>
2018-07-09 15:39:17 -0400

    attempting to parse json

--- main.go
+++ main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"fmt"
-	//"net/rpc"
 	"net/http"
-	//"github.com/ethereum/go-ethereum/rpc"
 	//"math/big"
 	"bytes"
 	"io/ioutil"
 	"time"
+	"encoding/json"
 )
+
+type Resp struct {
+	jsonrpc string
+	id int
+	result string
+}
 
 func main() {
 	// hard coded to geth running at address:port
 	url := "http://127.0.0.1:8545"
 
-	var jsonStr = []byte(`{"jsonrpc":"2.0","method":"eth_mining","params":[],"id":71}`)
+	var jsonStr = []byte(`{"jsonrpc":"2.0","method":"eth_newBlockFilter","params":[],"id":71}`)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
@@ -29,14 +34,26 @@ func main() {
 	fmt.Println("response Headers:", resp.Header)
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
+	//jsonBody := []byte(string(body))
 
+	var jsonBody = []byte(`
+		{"jsonrpc":"2.0","id":71,"result":"0x7eec0a227d852c28d4141c212578292b"}
+	`)
+
+	var respBody Resp
+	err = json.Unmarshal(jsonBody, &respBody)
+	if err != nil { fmt.Println(err) }
+	fmt.Println(respBody)
+
+        //jsonStr = []byte(`{"jsonrpc":"2.0","method":"eth_getFilterChanges","params":[],"id":71}`)
+	jsonStr = []byte(`{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":71}`)
 	ticker := time.NewTicker(500 * time.Millisecond)
 	go func() {
 		for t := range ticker.C {
 			fmt.Println(t)
-        		req, _ = http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-        		req.Header.Set("Content-Type", "application/json")
-        		resp, err := client.Do(req)
+			req, _ = http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+			req.Header.Set("Content-Type", "application/json")
+			resp, err := client.Do(req)
         		if err != nil {
                 		fmt.Println(err)
         		}
@@ -49,6 +66,6 @@ func main() {
 		}
 	}()
 
-	time.Sleep(20 * time.Second)
+	time.Sleep(60 * time.Second)
 	ticker.Stop()
 }
