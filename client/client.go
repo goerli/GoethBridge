658d8ac5d4e34cae06cf188653d9b3d99b818b4b
ebinks <elizabethjbinks@gmail.com>
2018-08-21 10:57:40 -0400

    restored fatal errors in client.go

--- client/client.go
+++ client/client.go
@@ -379,14 +379,14 @@ func Listen(chain *Chain, ac []*Chain, e *Events, doneClient chan bool, ks *keys
 			//fmt.Println("could not get block with ethclient.. trying http request")
 			blockNum, err := getBlockNumber(chain.Url)
 			if err != nil {
-				//log.Fatal(err)
+				log.Fatal(err)
 			}
 			if flags["v"] { fmt.Println("latest block at chain", chain.Id, ":", blockNum) }
 			fromBlock, _ = new(big.Int).SetString(blockNum[2:], 16)
 
 			blockRoot.Hash, err = getBlockRoot(chain.Url, blockNum)
 			if err != nil {
-				//log.Fatal(err)
+				log.Fatal(err)
 			}
 		} else if fromBlock != block.Number() {
 			if err != nil { log.Fatal(err) }
