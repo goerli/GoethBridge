6bcda076ba986f2b97cffb6625d9794363ee18c4
ebinks <elizabethjbinks@gmail.com>
2018-11-13 16:13:32 -0500

    added config for testnets

--- leth/main.go
+++ leth/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"github.com/ChainSafeSystems/leth/core"
 )
 
 func main() {
-	err := core.Migrate("default", "Bridge")
+	/*err := core.Migrate("default", "Bridge")
 	if err != nil {
 		fmt.Println("could not deploy Bridge.sol to default network")
-	}
+	}*/
+
+	// err := core.Migrate("default", "Home")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Home.sol to default network")
+ //    }
+
+	// err = core.Migrate("testnet", "Foreign")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Foreign.sol to ropsten")
+ //    }
+
+	// err := core.Migrate("kovan", "Foreign")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Foreign.sol to kovan")
+ //    }
+
+	// err := core.Migrate("ropsten", "Foreign")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Foreign.sol to ropsten")
+ //    }
+
+   	err := core.Migrate("rinkeby", "Home")
+    if err != nil {
+            fmt.Println("could not deploy Foreign.sol to rinkeby")
+    }
 }
