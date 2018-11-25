e64fdf87bba8d4a85bff2e0a51c42c4cd92773e9
ebinks <elizabethjbinks@gmail.com>
2018-11-25 14:44:26 -0500

    merging

--- client/transaction.go
+++ client/transaction.go
@@ -189,7 +189,7 @@ func FundBridge(chain *Chain, value *big.Int) {
 	nonce, err := client.PendingNonceAt(context.Background(), *chain.From)
 	chain.Nonce = nonce
 
-	tx := types.NewTransaction(chain.Nonce, *chain.Contract, value, uint64(4600000), chain.GasPrice, data)
+	tx := types.NewTransaction(chain.Nonce, *chain.Contract, value,  uint64(4600000), chain.GasPrice, data)
 	txSigned, err := keys.SignTxWithPassphrase(*from, chain.Password, tx, chain.Id)
 	if err != nil {
 		fmt.Println("could not sign tx")
--- leth/main.go
+++ leth/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/ChainSafeSystems/"
 )
 
 func main() {
@@ -8,4 +10,29 @@ func main() {
 	if err != nil {
 		fmt.Println("could not deploy Bridge.sol to default network")
 	}
+
+	// err := core.Migrate("default", "Home")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Home.sol to default network")
+ //    }
+
+	// err = core.Migrate("testnet", "Foreign")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Foreign.sol to ropsten")
+ //    }
+
+	// err := core.Migrate("kovan", "Foreign")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Foreign.sol to kovan")
+ //    }
+
+	// err = core.Migrate("ropsten", "Foreign")
+ //    if err != nil {
+ //            fmt.Println("could not deploy Foreign.sol to ropsten")
+ //    }
+
+   	// err := core.Migrate("rinkeby", "Foreign")
+    // if err != nil {
+    //         fmt.Println("could not deploy Foreign.sol to rinkeby")
+    // }
 }
