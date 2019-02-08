0d9442d681b1e621fb28d01a188acc8a8b592e2b
David Ansermino <david.ansermino@mail.utoronto.ca>
2019-02-08 14:15:01 -0500

    Added daily limit

--- solidity/Foreign/Foreign.sol
+++ solidity/Foreign/Foreign.sol
@@ -5,8 +5,14 @@ pragma solidity ^0.5.0;
  */
 
 contract Foreign {
-
+    // Per account daily limit
 	uint256 constant MAX_DEPOSIT = 100 ether;
+	// Bridge daily limit
+	uint256 constant MAX_PER_DAY = 1000000 ether;
+
+	address payable owner;
+	uint256 timestamp;
+	uint256 dailyTotal;
 
 	mapping(address => uint256) deposited; // how much ether that was deposited today
 	mapping(address => uint256) depositTime; // time from their first deposit today
@@ -15,10 +21,22 @@ contract Foreign {
 	event Deposit(address _recipient, uint _value, uint _toChain);
 
 	constructor() public {
+	    owner = msg.sender;
+	    timestamp = now;
 		emit ContractCreation(msg.sender);
 	}
 
 	function deposit(address _recipient, uint _toChain) public payable {
+	    if(now < timestamp + 1 days) {
+	        // Ensure we are not exceeding the limit
+	        assert(dailyTotal + msg.value <= MAX_PER_DAY);
+	        dailyTotal += msg.value;
+	    } else {
+	        // Reset total and timestamp
+	        timestamp = now;
+	        dailyTotal = 0;
+	    }
+
 		// if they haven't made a deposit in the last day, reset their amount and time
 		if (depositTime[msg.sender] < now + 1 days) {
 			depositTime[msg.sender] = now;
@@ -32,5 +50,12 @@ contract Foreign {
 		address(0).transfer(msg.value);
 		deposited[msg.sender] += msg.value;
 		emit Deposit(_recipient, msg.value, _toChain);
+	}
+
+	function withdraw(uint _amount) public returns (bool){
+	    require(msg.sender == owner);
+	    require(_amount <= address(this).balance);
+	    owner.transfer(_amount);
+	    return true;
 	}
 }
